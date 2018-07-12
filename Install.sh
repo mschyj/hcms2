83d9f51c8b154a24037ff52a6b80c6f27c39c10e
root <root@ecs-cluster-fb.novalocal>
2018-07-12 09:07:35 +0800

    fanbin-update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,4 +1,8 @@
 python setup.py install
+cd sdk-python-master
+pip install -r requirements.txt
+python setup.py install
+cd ..
 echo "|======================================================================|"
 echo " Installation is completed "
 echo " Please run the script Inithwcc.sh to setup your environment "     
