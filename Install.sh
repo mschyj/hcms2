cefec8e4ba0e0239b11c6a65859c706cac47c3c6
root <root@ecs-cluster-fb.novalocal>
2018-07-13 11:44:24 +0800

    fanbin-fix postPaid ecs bug

--- Install.sh
+++ Install.sh
@@ -2,6 +2,7 @@ python setup.py install
 cd pythonsdk
 pip install -r requirements.txt
 python setup.py install
+export PYTHONPATH=.:/usr/local/lib/python2.7/site-packages/openstack
 cd ..
 echo "|======================================================================|"
 echo " Installation is completed "
