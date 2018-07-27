fcdf61764439a0d57e6cfc8f36ccd90dc5763e9b
root <root@hwc.(none)>
2018-07-27 15:41:21 +0800

    chenyjie-fix sge sfs issue

--- StartHttp.sh
+++ StartHttp.sh
@@ -1,4 +1,4 @@
 ps -ef | grep SimpleHTTPServer | awk '{print $2}' | xargs kill -9
-cd /sfs/appstore
+cd /hwcc
 python -m SimpleHTTPServer 80 &>/dev/null &
 ps -ef | grep SimpleHTTPServer | grep -v grep
